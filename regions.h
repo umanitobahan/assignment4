2b21d3eec186e9a50990b0caa5f818c1dff9218a
Han Liu <liuh3410@woodchuck.cs.umanitoba.ca>
2017-03-29 02:32:30 -0500

    ADD HEAD FILE add a head file as a interface, include functions and type define

---
+++ regions.h
@@ -0,0 +1,17 @@
+#ifndef _REGISONS_H
+#define _REGIONS_H
+	
+	typedef enum { false, true } Boolean;
+
+	typedef unsigned short r_size_t;
+
+	Boolean rinit(const char *region_name, r_size_t region_size);
+	Boolean rchoose(const char *region_name);
+	const char *rchosen();
+	void *ralloc(r_size_t block_size);
+	r_size_t rsize(void *block ptr);
+	Boolean rfree(void *block_ptr);
+	void rdestory(const char *region_name);
+	void rdump();
+
+#endif
